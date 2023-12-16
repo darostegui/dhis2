e7e37956ae2d2b9f57897df55d523d9ccb6606a5
Diego Arostegui <darostegui@github.com>
2023-12-16 10:15:33 +0100

    Update get-dhis2-version.sh

--- get-dhis2-version.sh
+++ get-dhis2-version.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-API_VERSION=`curl -s http://localhost/api/33/system/info -u "admin:district" | jq -r .version` ## Get it
-echo $API_VERSION ## Print it
-logger -p local0.notice -t ${0##*/}[$$] DHIS VERSION at `date +"%D %T"` :  $API_VERSION ## log it
+if [[ -z $1 && -z $2 ]]; then
+ echo "USAGE bash get-dhis2-version.sh http://localhost admin:district"
+else
+ API_VERSION=`curl -s $1/api/33/system/info -u "$2" | jq -r .version` ## Get it
+ echo $API_VERSION ## Print it
+ logger -p local0.notice -t ${0##*/}[$$] DHIS VERSION at `date +"%D %T"` :  $API_VERSION ## log it
+fi
 
 ### Should return and log:
-### ./dhis-version.sh
+### ./get-dhis2-version.sh http://localhost admin:district
 ### 2.40.2
 ### tail -n 1 /var/log/syslog
 ### Dec 16 08:58:56 darostegui-backup -bash[1026859]: DHIS VERSION at 12/16/23 08:58:56 : 2.40.2
